42277e32aedc2c12b78baa5120ce0099b24b0436
shadowzen1978 <shadowzen1978@gmail.com>
2021-10-01 01:30:00 -0500

    cleaned up debug info script and feature branch workflow.

--- .github/scripts/git-get-last-commit-not-merge.sh
+++ .github/scripts/git-get-last-commit-not-merge.sh
@@ -5,10 +5,6 @@
 # this can filter out messages related to automation tasks that are not
 # relevant to material changes in the repo.
 
-#echo "Last 5 commit messages"
-#git --no-pager log --pretty=format:"%s" -n 5
-#echo ""
-
 LAST_COMMIT_MESSAGE=$(git --no-pager log --pretty=format:"%s" -n 1)
 
 echo -e "Last commit message is:\n $LAST_COMMIT_MESSAGE\n"
@@ -17,9 +13,6 @@ COUNTER=1
 while [[ "$LAST_COMMIT_MESSAGE" =~ ^Merge[[:space:]]branch.*$
           || "$LAST_COMMIT_MESSAGE" =~ ^chore:.*$ ]]; do
   echo "Last message is merge or change message: $LAST_COMMIT_MESSAGE"
-  echo -e "Debug info:\n counter is $COUNTER"
-  echo -e "Git command output: "
-  GIT_TRACE=1 git --no-pager log --pretty=format:"%s" -n 1 --skip=$COUNTER
   LAST_COMMIT_MESSAGE="$(git --no-pager log --pretty=format:"%s" -n 1 --skip=$COUNTER)"
   COUNTER=$((COUNTER+1))
 done
